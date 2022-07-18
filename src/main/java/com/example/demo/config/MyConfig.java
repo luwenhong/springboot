b3a0c77c3babd25a1910d07c6c636c1d9c9690f9
lll <2143758640@qq.com>
2022-07-18 21:44:45 +0800

    配置类更新和创建pet

--- src/main/java/com/example/demo/config/MyConfig.java
+++ src/main/java/com/example/demo/config/MyConfig.java
@@ -1,17 +1,26 @@
 package com.example.demo.config;
 
+import com.example.demo.pojo.Pet;
 import com.example.demo.pojo.User;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Import({User.class})//给容器中自动创建出这个类型的组件，调用无参构造函数，默认组件名为全类名
 @Configuration  //告訴springboot這是一個配置類===配置文件
 public class MyConfig {
+	
+	@ConditionalOnBean(Pet.class)   //当容器中有pet类时才注册user01
 	@Bean
 	//给容器中添加组件，以方法名作为组件的id（也可以在bean中另取名字），返回类型就是组件类型
 	//返回值，就是组件在容器中的实例
 	public User user01(){
 		return new User("lwn",18);
 	}
-}
+	
+	@Bean
+	public Pet pet(){
+		return new Pet("dog");
+}}
---
+++ src/main/java/com/example/demo/pojo/Pet.java
@@ -0,0 +1,20 @@
+package com.example.demo.pojo;
+
+public class Pet {
+	private String name;
+	
+	public Pet(String name) {
+		this.name = name;
+	}
+	
+	public Pet() {
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}

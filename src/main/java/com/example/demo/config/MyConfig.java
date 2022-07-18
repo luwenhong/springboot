d52713c90888616613faef879988ffc297cc73ed
lll <2143758640@qq.com>
2022-07-18 20:49:14 +0800

    添加配置类和实体类user

---
+++ src/main/java/com/example/demo/config/MyConfig.java
@@ -0,0 +1,7 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration  //告訴springboot這是一個配置類===配置文件
+public class MyConfig {
+}
---
+++ src/main/java/com/example/demo/pojo/User.java
@@ -0,0 +1,6 @@
+package com.example.demo.pojo;
+
+public class User {
+	private String name;
+	private int age;
+}

723455bda9453b2f564b3b91f60f9968107d9e48
lll <2143758640@qq.com>
2022-07-18 22:07:12 +0800

    配置类更新和创建pet

--- src/main/java/com/example/demo/config/MyConfig.java
+++ src/main/java/com/example/demo/config/MyConfig.java
@@ -4,7 +4,6 @@ import com.example.demo.pojo.Pet;
 import com.example.demo.pojo.User;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -12,7 +11,12 @@ import org.springframework.context.annotation.Import;
 @Configuration  //告訴springboot這是一個配置類===配置文件
 public class MyConfig {
 	
-	@ConditionalOnBean(Pet.class)   //当容器中有pet类时才注册user01
+	@Bean("pet")
+	public Pet pet01(){
+		return new Pet("dog");
+		
+	}
+	@ConditionalOnBean(name = "pet")   //当容器中有pet时才注册user01,注意要先注册pet后才生效
 	@Bean
 	//给容器中添加组件，以方法名作为组件的id（也可以在bean中另取名字），返回类型就是组件类型
 	//返回值，就是组件在容器中的实例
@@ -20,7 +24,5 @@ public class MyConfig {
 		return new User("lwn",18);
 	}
 	
-	@Bean
-	public Pet pet(){
-		return new Pet("dog");
-}}
+	
+}

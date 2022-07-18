5243bb013abf7807a9b70e1237196c647638b2ba
lll <2143758640@qq.com>
2022-07-18 21:39:34 +0800

    配置类更新

--- src/main/java/com/example/demo/config/MyConfig.java
+++ src/main/java/com/example/demo/config/MyConfig.java
@@ -3,11 +3,13 @@ package com.example.demo.config;
 import com.example.demo.pojo.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+@Import({User.class})//给容器中自动创建出这个类型的组件，调用无参构造函数，默认组件名为全类名
 @Configuration  //告訴springboot這是一個配置類===配置文件
 public class MyConfig {
 	@Bean
-	//给容器中添加组件，以方法名作为组件的id，返回类型就是组件类型
+	//给容器中添加组件，以方法名作为组件的id（也可以在bean中另取名字），返回类型就是组件类型
 	//返回值，就是组件在容器中的实例
 	public User user01(){
 		return new User("lwn",18);
